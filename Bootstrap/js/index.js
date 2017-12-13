0ba17aefed773da2ed1df8527869793a7f03c7b7
Unknown <skyzerzlol@gmail.com>
2017-12-13 23:30:49 +0200

    News page and fix small news box, haven't link yet

--- Bootstrap/js/index.js
+++ Bootstrap/js/index.js
@@ -1,6 +1,9 @@
 var ongoingmatches = document.getElementById("ongoing-matches");
 var upcomingmatches = document.getElementById("upcoming-matches");
 var finishedmatches = document.getElementById("finished-matches");
+var streamingbox = document.getElementById("streaming-box");
+var matchbox2 = document.getElementById("matchbox2");
+var streaming = document.getElementById("streaming");
 var ongoing = document.getElementById("ongoing");
 var upcoming = document.getElementById("upcoming");
 var finished = document.getElementById("finished");
@@ -9,6 +12,7 @@ function Ongoing() {
   ongoingmatches.style.display = "inline";
   upcomingmatches.style.display = "none";
   finishedmatches.style.display = "none";
+  streamingbox.style.display = "none";
 
   ongoing.className = "btn btn-primary";
   upcoming.className = "btn btn-secondary";
@@ -19,6 +23,8 @@ function Upcoming() {
   ongoingmatches.style.display = "none";
   upcomingmatches.style.display = "inline";
   finishedmatches.style.display = "none";
+  streamingbox.style.display = "none";
+
 
   ongoing.className = "btn btn-secondary";
   upcoming.className = "btn btn-primary";
@@ -29,11 +35,32 @@ function Finished() {
   ongoingmatches.style.display = "none";
   upcomingmatches.style.display = "none";
   finishedmatches.style.display = "inline";
+  streamingbox.style.display = "none";
+
 
   ongoing.className = "btn btn-secondary";
   upcoming.className = "btn btn-secondary";
   finished.className = "btn btn-primary";
 }
+
+function livestream(){
+  matchbox2.style.display = "none";
+  streamingbox.style.display = "inline";
+  document.getElementById('onlink').id = 'matchbox3';
+  document.getElementById('stream1').id = 'onlink';
+
+}
+
+function matchbox(){
+  matchbox2.style.display = "inline";
+  streamingbox.style.display = "none";
+  document.getElementById('onlink').id = 'stream1';
+  document.getElementById('matchbox3').id = 'onlink';
+
+
+}
+
+
 
 
 window.onload = Ongoing();
